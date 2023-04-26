2bef78b6f8f2fc27ae168ee7ba50a11d0c127533
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-04-26 22:02:25 +0500

    add mppt

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from filebrowser.sites import site
+
+# from filebrowser.sites import site
 
 urlpatterns = [
     path("admin/", admin.site.urls),
